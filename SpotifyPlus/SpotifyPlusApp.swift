774cade099f31ae5541a762e8f127b53b97bd989
Nick <nickrizzo76@protonmail.com>
2023-05-22 14:45:14 -0400

    Initial Commit

---
+++ SpotifyPlus/SpotifyPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotifyPlusApp.swift
+//  SpotifyPlus
+//
+//  Created by Nicholas Rizzo on 5/22/23.
+//
+
+import SwiftUI
+
+@main
+struct SpotifyPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
